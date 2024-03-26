f8d31ecb7c3b218cedca3722add236ae53bdb631
krithigavarshiniAK <110885881+krithigavarshiniAK@users.noreply.github.com>
2024-03-26 16:28:28 +0530

    Delete coordinates/src directory

--- coordinates/src/main/java/com/testing/coordinates/Service/CoordinatesService.java
+++
@@ -1,9 +0,0 @@
-package com.testing.coordinates.Service;
-
-import com.testing.coordinates.model.Coordinates;
-
-public interface CoordinatesService {
-    public double calculateDistance(Coordinates coordinates);
-
-    public double calculateDistance2(Coordinates coordinates, double lat2, double lon2);
-}

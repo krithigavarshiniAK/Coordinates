0ee4ca1ebc958fb045943d2e71595d2f65ab1b51
krithigavarshiniAK <110885881+krithigavarshiniAK@users.noreply.github.com>
2024-04-16 15:01:36 +0530

    new test cases

--- coordinates/src/test/java/com/testing/coordinates/CoordinatesApplicationTests.java
+++ coordinates/src/test/java/com/testing/coordinates/CoordinatesApplicationTests.java
@@ -19,41 +19,180 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ContextConfiguration(classes = CoordinatesApplication.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CoordinatesController.class)
 class CoordinatesApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private CoordinatesService mockCoordinatesService;
 
-	@Autowired
-	ObjectMapper objectMapper;
 
-	@MockBean
-	CoordinatesService coordinatesService;
+    @MockBean
+    TerminalDetails terminal;
 
-	@Test
-	public void demotest(){
-		assertTrue(true);
-	}
+    @Test
+    public void demotest() {
+        assertTrue(true);
+    }
+    @Test
+    void testCoord() throws Exception {
+        final MockHttpServletResponse response = mockMvc.perform(get("/api/v3/Coordinates/coord-test")
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse();
 
-	@Test
-	public void testMyEndpoint() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v3/Coordinates/coord-test"))
-				.andExpect(status().isOk())
-				.andExpect(content().string("Coordinates"));
-	}
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo("Coordinates");
+    }
 
-	@Test
-	public void testCalculateDistance() throws Exception {
-		Coordinates coordinates = new Coordinates(51.5007, 0.1246, 40.6892, 74.0445);
-		when(coordinatesService.calculateDistance(coordinates)).thenReturn(5574.840456848554);
+//    @Test
+//    void testCalculateDistance1_Positive_testCase() throws Exception {
+//        when(mockCoordinatesService.getDistanceWithFixedSourceAndDestination(any(Coordinates.class))).thenReturn(127171.1223524084);
+//
+//        String requestBody = "{\"latitude1\": 13.011760, \"longitude1\": 80.221481, \"latitude2\": 11.941591, \"longitude2\": 79.808311}";
+//
+//        final MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance")
+//                        .content(requestBody).contentType(MediaType.APPLICATION_JSON_VALUE)
+//                        .accept(MediaType.APPLICATION_JSON_VALUE))
+//                .andReturn().getResponse();
+//
+//        verify(mockCoordinatesService).getDistanceWithFixedSourceAndDestination(any(Coordinates.class));
+//
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+//        assertThat(response.getContentAsString()).isEqualTo("127171.1223524084");
+//    }
+//
+//    @Test
+//    void testCalculateDistance1_Missing_Parameters() throws Exception{
+//        when(mockCoordinatesService.getDistanceWithFixedSourceAndDestination(any(Coordinates.class)))
+//                .thenThrow(new RuntimeException("Error calculating distance"));
+//
+//        String requestBody = "{\"latitude1\": , \"longitude1\": 80.221481, \"latitude2\": 13.008430, \"longitude2\": 80.207169}";
+//
+//        MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance")
+//                        .content(requestBody).contentType(MediaType.APPLICATION_JSON_VALUE)
+//                        .accept(MediaType.APPLICATION_JSON_VALUE))
+//                .andReturn().getResponse();
+//
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+//        System.err.println("Error occurred due to missing parameter in the input");
+//    }
+//
+//    @Test
+//    void testCalculateDistance1_Negative_testCase() throws Exception {
+//        when(mockCoordinatesService.getDistanceWithFixedSourceAndDestination(any(Coordinates.class))).thenReturn(1594.1693658154443);
+//
+//        String requestBody = "{\"latitude1\": -13.011760, \"longitude1\": -80.221481, \"latitude2\": -13.008430, \"longitude2\": -80.207169}";
+//
+//        final MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance")
+//                        .content(requestBody).contentType(MediaType.APPLICATION_JSON_VALUE)
+//                        .accept(MediaType.APPLICATION_JSON_VALUE))
+//                .andReturn().getResponse();
+//
+//        verify(mockCoordinatesService).getDistanceWithFixedSourceAndDestination(any(Coordinates.class));
+//
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+//        assertThat(response.getContentAsString()).isEqualTo("1594.1693658154443");
+//    }
 
-		mockMvc.perform(MockMvcRequestBuilders.post("/api/v3/Coordinates/distance")
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.content(objectMapper.writeValueAsString(coordinates)))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string(String.valueOf(0.0)));
-	}
+//    @Test
+//    void testCalculateDistance2_Positive_testCase() throws Exception {
+//        when(mockCoordinatesService.getDistanceWithFixedSource(any(Coordinates.class), anyDouble(), anyDouble()))
+//                .thenReturn(1594.1693658154443); // Mock distance value
+//
+//        String requestBody = "{\"latitude1\": 13.011760, \"longitude1\": 80.221481}";
+//
+//        MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance2")
+//                        .content(requestBody).contentType(MediaType.APPLICATION_JSON_VALUE)
+//                        .param("latitude2", "13.008430").param("longitude2", "80.207169"))
+//                .andReturn().getResponse();
+//
+//        verify(mockCoordinatesService).getDistanceWithFixedSource(any(Coordinates.class), anyDouble(), anyDouble());
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+//        assertThat(response.getContentAsString()).isEqualTo("1594.1693658154443");
+//    }
+//
+//    @Test
+//    void testCalculateDistance2_Missing_Parameter() throws Exception{
+//        when(mockCoordinatesService.getDistanceWithFixedSource(any(Coordinates.class), anyDouble(), anyDouble()))
+//                .thenThrow(new RuntimeException("Error calculating distance"));
+//
+//        String requestBody = "{\"latitude1\": 13.011760, \"longitude1\": 80.221481}";
+//
+//        MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance2")
+//                        .content(requestBody).contentType(MediaType.APPLICATION_JSON_VALUE)
+//                        .param("latitude2", " ").param("longitude2", "80.207169"))
+//                .andReturn().getResponse();
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+//        System.err.println("Error occurred due to missing parameter in the input");
+//    }
+//
+//    @Test
+//    void testCalculateDistance2_Negative_testCase() throws Exception{
+//        when(mockCoordinatesService.getDistanceWithFixedSource(any(Coordinates.class), anyDouble(), anyDouble()))
+//                .thenReturn(1594.1693658154443); // Mock distance value
+//
+//        String requestBody = "{\"latitude1\": -13.011760, \"longitude1\": -80.221481}";
+//
+//        MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance2")
+//                        .content(requestBody).contentType(MediaType.APPLICATION_JSON_VALUE)
+//                        .param("latitude2", "-13.008430").param("longitude2", "-80.207169"))
+//                .andReturn().getResponse();
+//
+//        verify(mockCoordinatesService).getDistanceWithFixedSource(any(Coordinates.class), anyDouble(), anyDouble());
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+//        assertThat(response.getContentAsString()).isEqualTo("1594.1693658154443");
+//    }
+
+//    @Test
+//    void testCalculateDistance3_Positive_testCase() throws Exception {
+//        when(mockCoordinatesService.getDistanceWithDynamicSourceAndDestination(anyDouble(), anyDouble(), anyDouble(), anyDouble()))
+//                .thenReturn(422105.23754448025); // Mock distance value
+//
+//        MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance3")
+//                        .param("latitude1", "9.9252").param("longitude1", "78.1198")
+//                        .param("latitude2", "13.0826").param("longitude2", "80.2707"))
+//                .andReturn().getResponse();
+//
+//        verify(mockCoordinatesService).getDistanceWithDynamicSourceAndDestination(anyDouble(), anyDouble(), anyDouble(), anyDouble());
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+//        assertThat(response.getContentAsString()).isEqualTo("422105.23754448025");
+//    }
+//    @Test
+//    void testCalculateDistance3_Missing_Parameters() throws Exception {when(mockCoordinatesService.getDistanceWithDynamicSourceAndDestination(anyDouble(), anyDouble(), anyDouble(), anyDouble()))
+//                .thenThrow(new RuntimeException("Error calculating distance"));
+//
+//        MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance2")
+//                        .param("latitude1", " ").param("longitude1", "78.1198")
+//                        .param("latitude2", "13.0826").param("longitude2", "80.2707"))
+//                .andReturn().getResponse();
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+//        System.err.println("Error occurred due to missing parameter in the input");
+//    }
+//
+//    @Test
+//    void testCalculateDistance3_Negative_testCase() throws Exception
+//    {
+//        when(mockCoordinatesService.getDistanceWithDynamicSourceAndDestination(anyDouble(), anyDouble(), anyDouble(), anyDouble()))
+//                .thenReturn(422105.23754448025);
+//
+//        MockHttpServletResponse response = mockMvc.perform(post("/api/v3/Coordinates/distance3")
+//                        .param("latitude1", "-9.9252").param("longitude1", "-78.1198")
+//                        .param("latitude2", "-13.0826").param("longitude2", "-80.2707"))
+//                .andReturn().getResponse();
+//
+//        verify(mockCoordinatesService).getDistanceWithDynamicSourceAndDestination(anyDouble(), anyDouble(), anyDouble(), anyDouble());
+//
+//        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+//        assertThat(response.getContentAsString()).isEqualTo("422105.23754448025");
+//    }
 }
